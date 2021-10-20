5b3b727327158b0b209a9a28c616c12aa8bb1419
Luiz Henrique <lpansarini@gmail.com>
2021-10-20 20:00:01 -0300

    add framer

--- src/components/MessageList/index.tsx
+++ src/components/MessageList/index.tsx
@@ -1,8 +1,10 @@
-import styles from "./styles.module.scss";
-import logoImg from "../../assets/logo.svg";
 import { useEffect, useState } from "react";
 import { api } from "../../services/api";
+import styles from "./styles.module.scss";
 import io from "socket.io-client";
+
+import logoImg from "../../assets/logo.svg";
+import { motion } from "framer-motion";
 
 type Message = {
   id: string;
@@ -25,7 +27,13 @@ export function MessageList() {
   const [messages, setMessages] = useState<Message[]>([]);
 
   useEffect(() => {
-    setInterval(() => {
+    api.get<Message[]>("messages/last3").then((response) => {
+      setMessages(response.data);
+    });
+  }, []);
+
+  useEffect(() => {
+    const timer = setInterval(() => {
       if (messagesQueue.length > 0) {
         setMessages((prevState) =>
           [messagesQueue[0], prevState[0], prevState[1]].filter(Boolean)
@@ -34,29 +42,38 @@ export function MessageList() {
         messagesQueue.shift();
       }
     }, 3000);
-  }, []);
 
-  useEffect(() => {
-    api.get<Message[]>("messages/last3").then((response) => {
-      setMessages(response.data);
-    });
+    return () => clearInterval(timer);
   }, []);
 
   return (
     <div className={styles.messageListWrapper}>
       <img className={styles.logo} src={logoImg} alt="DoWhile 2021" />
+
       <ul className={styles.messageList}>
-        {messages.map((message) => (
-          <li className={styles.message} key={message.id}>
-            <p className={styles.messageContent}>{message.text}</p>
-            <div className={styles.messageUser}>
-              <div className={styles.userImage}>
-                <img src={message.user.avatar_url} alt={message.user.name} />
+        {messages.map((message) => {
+          return (
+            <motion.li
+              initial={{ opacity: 0, translateX: -50 }}
+              animate={{ opacity: 1, translateX: 0 }}
+              transition={{
+                type: "spring",
+                stiffness: 260,
+                damping: 20,
+              }}
+              className={styles.message}
+              key={message.id}
+            >
+              <p className={styles.messageContent}>{message.text}</p>
+              <div className={styles.messageUser}>
+                <div className={styles.userImage}>
+                  <img src={message.user.avatar_url} alt={message.user.name} />
+                </div>
+                <span>{message.user.name}</span>
               </div>
-              <span>{message.user.name}</span>
-            </div>
-          </li>
-        ))}
+            </motion.li>
+          );
+        })}
       </ul>
     </div>
   );
