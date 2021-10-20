7d308bf8c2adcd0651dbe7ccd4ca8903486ce799
Luiz Pansarini <lpansarini@gmail.com>
2021-10-19 21:03:10 -0300

    final project

--- src/components/LoginBox/index.tsx
+++ src/components/LoginBox/index.tsx
@@ -1,11 +1,15 @@
 import { VscGithubInverted } from "react-icons/vsc";
+
+import { useAuth } from "../../hooks/useAuth";
 import styles from "./styles.module.scss";
 
 export function LoginBox() {
+  const { signInUrl } = useAuth();
+
   return (
     <div className={styles.loginBoxWrapper}>
       <strong>Entre e compartilhe sua mensagem</strong>
-      <a href="#" className={styles.signWithGithubButton}>
+      <a href={signInUrl} className={styles.signWithGithubButton}>
         <VscGithubInverted size={24} />
         Entrar com Github
       </a>
